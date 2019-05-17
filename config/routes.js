920ea8591b4002b57e870e49538cc5650dcde8b0
cammac <camm@rocktechcomputer.com>
2019-05-17 09:06:25 -0700

    add login and register, refactored secrets files

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
+const Users = require('../users/users-model')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,11 +11,36 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(added => {
+      res.status(201).json(added)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}, here's a token`, token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 }
 
 function getJokes(req, res) {
